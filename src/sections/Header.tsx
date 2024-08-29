a45e61d5967d2061d34b8dec17f44c538e686c39
Safdar Ali Shah <programmersafdar@live.com>
2024-08-29 07:33:33 +0500

    Header Section, Hero Section, project structured Done, whle still working on Hero Section

---
+++ src/sections/Header.tsx
@@ -0,0 +1,42 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import MenuIcon from "@/assets/menu.svg";
+import Link from "next/link";
+export const Header = () => {
+  return (
+    <header className="sticky top-0 backdrop-blur-sm">
+      <div className="flex justify-center gap-3 items-center py-3 bg-black text-white">
+        <p className="text-white/60 hidden md:block">
+          Streamline/ Automize your workflow and boost your productivity
+        </p>
+
+        <div className="flex justify-center items-center gap-1">
+          <p className="font-semibold">Get Started for Free!</p>
+          <ArrowRight className="h-5 w-5 inline-flex justify-center items-center" />
+        </div>
+      </div>
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image
+              src={Logo}
+              alt="SaaS landing Page Logo"
+              width={40}
+              height={40}
+            ></Image>
+            <MenuIcon className="w-4 h-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <Link href="">About</Link>
+              <Link href="">Features</Link>
+              <Link href="">Customers</Link>
+              <Link href="">Updates</Link>
+              <Link href="">Help</Link>
+              <button className="btn btn-primary">Get for Free!</button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
