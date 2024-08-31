5d8a88745a07f27af38ecb1fb9e7bf43f9b12a1a
Safdar Ali Shah <programmersafdar@live.com>
2024-08-31 07:34:48 +0500

    Logos Added

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
 export const LogoTicker = () => {
-  return "Logo Ticker";
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className="flex">
+            <Image src={acmeLogo} alt="Acme Logo" />
+            <Image src={quantLogo} alt="Quant Logo" />
+            <Image src={echoLogo} alt="Echo Logo" />
+            <Image src={celestialLogo} alt="Celestial Logo" />
+            <Image src={pulseLogo} alt="Pulse Logo" />
+            <Image src={apexLogo} alt="Apex Logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
