444366a83d2e169f763112b57dc61527e77bd251
Safdar Ali Shah <programmersafdar@live.com>
2024-08-31 07:46:24 +0500

    Logo Ticer Finished Now Product Showcase started

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -8,15 +8,41 @@ import Image from "next/image";
 export const LogoTicker = () => {
   return (
     <>
-      <div>
+      <div className="py-8 md:py-12 bg-white">
         <div className="container">
-          <div className="flex">
-            <Image src={acmeLogo} alt="Acme Logo" />
-            <Image src={quantLogo} alt="Quant Logo" />
-            <Image src={echoLogo} alt="Echo Logo" />
-            <Image src={celestialLogo} alt="Celestial Logo" />
-            <Image src={pulseLogo} alt="Pulse Logo" />
-            <Image src={apexLogo} alt="Apex Logo" />
+          <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+            <div className="flex gap-14 flex-none">
+              <Image
+                src={acmeLogo}
+                alt="Acme Logo"
+                className="logo-tickers-image"
+              />
+              <Image
+                src={quantLogo}
+                alt="Quant Logo"
+                className="logo-tickers-image"
+              />
+              <Image
+                src={echoLogo}
+                alt="Echo Logo"
+                className="logo-tickers-image"
+              />
+              <Image
+                src={celestialLogo}
+                alt="Celestial Logo"
+                className="logo-tickers-image"
+              />
+              <Image
+                src={pulseLogo}
+                alt="Pulse Logo"
+                className="logo-tickers-image"
+              />
+              <Image
+                src={apexLogo}
+                alt="Apex Logo"
+                className="logo-tickers-image"
+              />
+            </div>
           </div>
         </div>
       </div>
