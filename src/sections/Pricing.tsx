069e017d2bae6f4137c8416bbb58591933fad0ff
Safdar Ali Shah <programmersafdar@live.com>
2024-09-02 09:47:04 +0500

    Pricing Checking tailwind added

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from "@/assets/check.svg";
+import twMerge from "tailwind-merge";
 const pricingTiers = [
   {
     title: "Free",
@@ -53,13 +55,60 @@ const pricingTiers = [
 export const Pricing = () => {
   return (
     <>
-      <section>
+      <section className="py-24">
         <div className="container">
           <h2 className="section-title">Pricing</h2>
-          <p className="section-description">
+          <p className="section-description mt-5">
             Free forever,Upgrade for unlimited tasks, better security, and
             exclusive features .
           </p>
+          <div>
+            {pricingTiers.map(
+              ({
+                title,
+                monthlyPrice,
+                buttonText,
+                popular,
+                inverse,
+                features,
+              }) => (
+                <div className="p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px-14px_#EAEAEA]">
+                  <div className="flex justify-between">
+                    <h3 className="text-lg font-bold text-black/50 ">
+                      {title}
+                    </h3>
+                    {popular === true && (
+                      <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                        <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                          Popular
+                        </span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="flex items-baseline gap-1 mt[30px]">
+                    <span className="text-4xl font-bold tracking-tighter leading-none">
+                      ${monthlyPrice}
+                    </span>
+                    <span className="tracking-tight font-bold text-black/50">
+                      /monthly
+                    </span>
+                  </div>
+                  <button className="btn btn-primary w-full mt-[30px]">
+                    {buttonText}
+                  </button>
+                  <ul className="flex flex-col gap-5 mt-8">
+                    {features.map((feature) => (
+                      <li className="text-sm flex items-center gap-4">
+                        <CheckIcon className="w-6 h-6" />
+                        <span>{feature}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )
+            )}
+          </div>
         </div>
       </section>
     </>
