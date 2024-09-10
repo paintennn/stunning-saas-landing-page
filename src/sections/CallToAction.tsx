b95bf6c3058d1be69fb23f6ef2e64e12d1145b7b
Safdar Ali Shah <programmersafdar@live.com>
2024-09-10 23:06:09 +0500

    Images added to Action Section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -6,11 +6,25 @@ export const CallToAction = () => {
   return (
     <section className="py-24 bg-gradient-to-b from-white to-[#D2DCFF]">
       <div className="container">
-        <h2 className="section-title">Sign up for Free Today!</h2>
-        <p className="section-description mt-5">
-          celebrate the joy of accomplishment with an app designed to track your
-          progress and motivates your efforts.
-        </p>
+        <div className="relative">
+          <h2 className="section-title">Sign up for Free Today!</h2>
+          <p className="section-description mt-5">
+            celebrate the joy of accomplishment with an app designed to track
+            your progress and motivates your efforts.
+          </p>
+          <Image
+            src={StarImage}
+            alt="Star Image"
+            width={360}
+            className="absolute lg:-left-[350px]"
+          />
+          <Image
+            src={SpringImage}
+            alt="Star Image"
+            width={360}
+            className="hidden absolute "
+          />
+        </div>
         <div className="flex justify-center gap-6 mt-10">
           <button className="btn btn-primary">Get for free</button>
           <button className="btn btn-text border">
