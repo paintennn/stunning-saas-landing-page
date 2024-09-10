30d8d0234db299a3033470a148830dbfd05abfa6
Safdar Ali Shah <programmersafdar@live.com>
2024-09-10 22:51:16 +0500

    Almost Call to Action Done

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,8 +1,22 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import StarImage from "@/assets/star.png";
+import SpringImage from "@/assets/spring.png";
+import Image from "next/image";
 export const CallToAction = () => {
   return (
-    <section>
+    <section className="py-24 bg-gradient-to-b from-white to-[#D2DCFF]">
       <div className="container">
-        <h2>Sign up for Free Today!</h2>
+        <h2 className="section-title">Sign up for Free Today!</h2>
+        <p className="section-description mt-5">
+          celebrate the joy of accomplishment with an app designed to track your
+          progress and motivates your efforts.
+        </p>
+        <div className="flex justify-center gap-6 mt-10">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text border">
+            <span>Learn more</span> <ArrowRight className="h-5 w-5" />
+          </button>
+        </div>
       </div>
     </section>
   );
