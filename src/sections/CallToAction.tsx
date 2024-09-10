ec3c3d6a2e4ef195814a65c1a3900891358e9108
Safdar Ali Shah <programmersafdar@live.com>
2024-09-10 09:25:19 +0500

    BreakingBad

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,9 @@
 export const CallToAction = () => {
-  return <div>Call to Action Started</div>;
+  return (
+    <section>
+      <div className="container">
+        <h2>Sign up for Free Today!</h2>
+      </div>
+    </section>
+  );
 };
