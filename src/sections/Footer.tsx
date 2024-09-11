5715714f60a49a1ec34d948b32c5f7244fd00c43
Safdar Ali Shah <programmersafdar@live.com>
2024-09-11 09:19:29 +0500

    Section: Footer> Elements Added Now adding CSS

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,31 @@
+import logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import SocialX from "@/assets/social-x.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+import SocialLinkedin from "@/assets/social-linkedin.svg";
+
 export const Footer = () => {
   return (
-    <section>
-      <div>Footer</div>
-    </section>
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute ">
+          <Image src={logo} alt="SaaS Logo" height={40} className="relative" />
+        </div>
+        <nav className="text-white flex flex-col">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="">
+          <SocialPin /> <SocialInsta /> <SocialYoutube /> <SocialLinkedin />
+        </div>
+        <p>&copy 2024 Company, Inc. All right reserved.</p>
+      </div>
+    </footer>
   );
 };
