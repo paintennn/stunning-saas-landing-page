047448c6ea30b4072041757f251d8502081455db
Safdar Ali Shah <programmersafdar@live.com>
2024-09-13 09:14:22 +0500

    Animation: Pricing Section Animation Added at Popular Tag.

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import CheckIcon from "@/assets/check.svg";
 import { twMerge } from "tailwind-merge";
 const pricingTiers = [
@@ -89,9 +91,18 @@ export const Pricing = () => {
                     </h3>
                     {popular === true && (
                       <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                        <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                        <motion.span
+                          animate={{ backgroundPositionX: "-100%" }}
+                          transition={{
+                            repeat: Infinity,
+                            ease: "linear",
+                            duration: 1,
+                            repeatType: "loop",
+                          }}
+                          className="font-sm bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF)] [background-size:200%] text-transparent bg-clip-text"
+                        >
                           Popular
-                        </span>
+                        </motion.span>
                       </div>
                     )}
                   </div>
